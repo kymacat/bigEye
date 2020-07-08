cd8faa7c1e2e83d933aae7e7777490f48f14a76d
Vlad Yandola <vladan2000.9@mail.ru>
2020-07-08 19:18:23 +0500

    Design corrections

--- Big eye/PresentationLayer/Modules/StatisticsViewController/StatisticsViewController.swift
+++ Big eye/PresentationLayer/Modules/StatisticsViewController/StatisticsViewController.swift
@@ -145,8 +145,8 @@ class StatisticsViewController: UIViewController {
         totalVisitDataEntry.value = visitsAndPasses.visits
         totalsPassDataEntry.value = visitsAndPasses.passes
         updateChartData(chart: statView.mainChartView, entries: mainDataEntries, colors: [
-            UIColor(red: 60/255, green: 232/255, blue: 198/255, alpha: 1),
-            UIColor(red: 255/255, green: 133/255, blue: 133/255, alpha: 1)
+            UIColor(red: 59/255, green: 200/255, blue: 238/255, alpha: 1),
+            UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1)
         ])
         
         
@@ -155,8 +155,8 @@ class StatisticsViewController: UIViewController {
         bestSubjectVisitsDataEntry.value = bestSubjectVisitsAndPasses.visitsAndPasses.visits
         bestSubjectPassesDataEntry.value = bestSubjectVisitsAndPasses.visitsAndPasses.passes
         updateChartData(chart: statView.bestJubjectChartView, entries: bestSubjectDataEntries, colors: [
-            UIColor(red: 140/255, green: 255/255, blue: 140/255, alpha: 1),
-            UIColor(red: 255/255, green: 148/255, blue: 81/255, alpha: 1)
+            UIColor(red: 69/255, green: 215/255, blue: 216/255, alpha: 1),
+            UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1)
         ])
         
         let badSubjectVisitsAndPasses = model.badSubjectVisitsAndPasses(model: data)
@@ -164,8 +164,8 @@ class StatisticsViewController: UIViewController {
         badSubjectVisitsDataEntry.value = badSubjectVisitsAndPasses.visitsAndPasses.visits
         badSubjectPassesDataEntry.value = badSubjectVisitsAndPasses.visitsAndPasses.passes
         updateChartData(chart: statView.badJubjectChartView, entries: badSubjectDataEntries, colors: [
-            UIColor(red: 73/255, green: 255/255, blue: 120/255, alpha: 1),
-            UIColor(red: 255/255, green: 55/255, blue: 111/255, alpha: 1)
+            UIColor(red: 42/255, green: 146/255, blue: 217/255, alpha: 1),
+            UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1)
         ])
         
         let bestStudentVisitsAndPasses = model.bestStudentVisitsAndPasses(model: data)
@@ -173,8 +173,8 @@ class StatisticsViewController: UIViewController {
         bestStudentVisitsDataEntry.value = bestStudentVisitsAndPasses.visitsAndPasses.visits
         bestStudentPassesDataEntry.value = bestStudentVisitsAndPasses.visitsAndPasses.passes
         updateChartData(chart: statView.bestStudentChartView, entries: bestStudentDataEntries, colors: [
-            UIColor(red: 23/255, green: 255/255, blue: 247/255, alpha: 1),
-            UIColor(red: 255/255, green: 61/255, blue: 147/255, alpha: 1)
+            UIColor(red: 175/255, green: 216/255, blue: 220/255, alpha: 1),
+            UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1)
         ])
         
         let badStudentVisitsAndPasses = model.badStudentVisitsAndPasses(model: data)
@@ -182,8 +182,8 @@ class StatisticsViewController: UIViewController {
         badStudentVisitsDataEntry.value = badStudentVisitsAndPasses.visitsAndPasses.visits
         badStudentPassesDataEntry.value = badStudentVisitsAndPasses.visitsAndPasses.passes
         updateChartData(chart: statView.badStudentChartView, entries: badStudentDataEntries, colors: [
-            UIColor(red: 172/255, green: 221/255, blue: 176/255, alpha: 1),
-            UIColor(red: 255/255, green: 172/255, blue: 176/255, alpha: 1)
+            UIColor(red: 21/255, green: 84/255, blue: 138/255, alpha: 1),
+            UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1)
         ])
         
     }
@@ -200,7 +200,8 @@ class StatisticsViewController: UIViewController {
         object.setValueFormatter(DefaultValueFormatter(formatter: pFormatter))
         object.setValueFont(.systemFont(ofSize: 14, weight: .bold))
         object.setValueTextColor(.black)
-        chartDataSet.colors = colors 
+        
+        chartDataSet.colors = colors
         
         chart.data = object
         
